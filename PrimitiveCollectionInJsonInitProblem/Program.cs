373e391f519b8bf04f90bcbc64684e5070a5bf7c
Daniel Boos <JejuniProgramming@gmail.com>
2023-11-15 18:14:50 +0100

    Add project files.

---
+++ PrimitiveCollectionInJsonInitProblem/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimitiveCollectionInJsonInitProblem;
+
+await using (var ctx = new TestContext())
+{
+    await ctx.Database.EnsureDeletedAsync();
+    await ctx.Database.EnsureCreatedAsync();
+}
+
+await using (var ctx = new TestContext())
+{
+    var user = new Pub("MyPub")
+    {
+        Visits = new Visits
+        {
+            LocationTag = "tag",
+            DaysVisited = new List<DateOnly> { new(2023, 1, 1) }
+        }
+    };
+
+    ctx.Pubs.Add(user);
+    await ctx.SaveChangesAsync();
+}
+
+await using (var ctx = new TestContext())
+{
+    var pubs = await ctx.Pubs.ToListAsync();
+    var pub1 = await ctx.Pubs
+        .Where(u => u.Visits.DaysVisited.Contains(new DateOnly(2023, 1, 1)))
+        .FirstOrDefaultAsync();
+}
---
+++ PrimitiveCollectionInJsonInitProblem/TestContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrimitiveCollectionInJsonInitProblem;
+
+public class Pub
+{
+    public Pub(string name)
+    {
+        Name = name;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Visits Visits { get; set; } = null!;
+}
+
+public class Visits
+{
+    public string? LocationTag { get; set; }
+    public List<DateOnly> DaysVisited { get; init; } = null!; // Change this from `init` to `set` and it works
+}
+
+internal class TestContext : DbContext
+{
+    public DbSet<Pub> Pubs => Set<Pub>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=localhost,5433;Initial Catalog=PrimitiveCollectionsProblem;User Id=sa;Password=Pass@word;TrustServerCertificate=true");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pub>(
+            b =>
+            {
+                b.OwnsOne(e => e.Visits).ToJson();
+            });
+    }
+}
